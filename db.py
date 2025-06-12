eddcaf9b6f6556ff031c662b184d5f09fd7f78f5
gabriel <guyex1996@gmail.com>
2025-06-12 16:13:19 +0100

    modified db.py

--- db.py
+++ db.py
@@ -1,9 +1,10 @@
 from sqlalchemy.ext.asyncio import create_async_engine, AsyncSession
 from sqlalchemy.orm import sessionmaker, declarative_base
+from decouple import config
 
+#DATABASE_URL = "postgresql+asyncpg://postgres:postgres@localhost:5432/new_db"
 
-DATABASE_URL = "postgresql+asyncpg://postgres:postgres@localhost:5432/new_db"
-
+DATABASE_URL = config("DATABASE_URL")
 
 
 engine = create_async_engine(DATABASE_URL, echo=False)
