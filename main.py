a533960d669f9759d07bdef94cb9b397f46586ac
gabriel <guyex1996@gmail.com>
2025-07-10 01:27:13 +0100

    railway fix

--- main.py
+++ main.py
@@ -434,3 +434,9 @@ async def undo_application(cv_id: UUID, job_id: UUID):
         return {"detail": "Application successfully withdrawn"}
 
 
+
+if __name__ == "__main__":
+    import uvicorn
+    import os
+    port = int(os.environ.get("PORT", 8000))
+    uvicorn.run("main:app", host="0.0.0.0", port=port)
